5f5f1a0878edb312b8902351eedef1a3648313d4
Manuel Prinz <manuel.prinz@pinguinkiste.de>
2023-09-07 21:29:02 +0200

    Add Gradle toolchain configuration

--- buildSrc/src/main/kotlin/iddd-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/iddd-conventions.gradle.kts
@@ -6,3 +6,9 @@ dependencies {
     implementation(group = "org.slf4j", name = "slf4j-api", version = "1.5.8")
     testImplementation(group = "junit", name = "junit", version = "4.8.2")
 }
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
