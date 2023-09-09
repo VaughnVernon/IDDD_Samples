3b9e3f6953069853e30c6e32aca09e974e262270
Manuel Prinz <manuel.prinz@pinguinkiste.de>
2023-09-09 13:40:56 +0200

    Fix version selection for broken dependency

--- buildSrc/src/main/kotlin/iddd-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/iddd-conventions.gradle.kts
@@ -5,6 +5,21 @@ plugins {
 dependencies {
     implementation(group = "org.slf4j", name = "slf4j-api", version = "1.5.8")
     testImplementation(group = "junit", name = "junit", version = "4.8.2")
+
+    dependencies {
+        constraints {
+            implementation(group = "javax.transaction", name = "jta") {
+                version {
+                    require("[1.1,2.0[")
+                    prefer("1.1")
+                }
+                because(
+                    "Older versions have licensing issues are were removed from Maven Central." +
+                            "See https://central.sonatype.org/faq/cannot-download-javax-transaction/."
+                )
+            }
+        }
+    }
 }
 
 java {
